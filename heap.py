515b74f03d9583bd72c42ba095e9439b20be9575
Keshav Agarwal <keshavagarwal123@gmail.com>
2020-06-12 10:50:07 +0800

    Implementation of a mx Heap

---
+++ heap.py
@@ -0,0 +1,33 @@
+class Heap:
+    def __init__(self, lstr=None):
+        self.heap = []
+        if isinstance(lstr, list):
+            self.heap = lstr
+            self.buildheap()
+
+    def __repr__(self):
+        return " ,".join(str(i) for i in self.heap)
+    
+    def buildheap(self):
+        n = len(self.heap)
+        for i in range(int(n/2-1), -1, -1):
+            self.heapify(i, n)
+
+    def heapify(self, i, n):
+        left = 2 * i + 1
+        right = 2 * i + 2
+        largest = None
+        
+        if left < n and self.heap[left] > self.heap[i]:
+            largest = left
+        if right < n and self.heap[right] > self.heap[left]:
+            largest = right
+        
+        if largest:
+            self.heap[i], self.heap[largest] = self.heap[largest], self.heap[i]
+            self.heapify(largest, n)
+
+
+
+hp = Heap([5, 3, 16, 2, 10, 14])
+print(hp)
